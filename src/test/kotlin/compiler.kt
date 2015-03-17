932a4ad9725adaf07913acc0f09dacea3d2b295a
Vladimir Zarypov <krre31@gmail.com>
2015-03-17 16:19:03 +0200

    getted filepath to test aml file

--- src/test/kotlin/compiler.kt
+++ src/test/kotlin/compiler.kt
@@ -3,5 +3,6 @@ package kotloid.test.compiler
 import kotloid.compiler.AmlObject
 
 fun main(args: Array<String>) {
-    val obj = AmlObject("filename")
+    val amlPath = System.getProperty("user.dir") + "/src/test/aml/MainActivity.aml"
+    val obj = AmlObject(amlPath)
 }
