5bea49a9aef848708a6a2b7462d623a92b106700
Vladimir Zarypov <krre31@gmail.com>
2015-03-17 20:23:52 +0200

    changed character "/" in filepath by File.separator

--- src/test/kotlin/compiler.kt
+++ src/test/kotlin/compiler.kt
@@ -1,8 +1,10 @@
 package kotloid.test.compiler
 
 import kotloid.compiler.AmlObject
+import java.io.File
 
 fun main(args: Array<String>) {
-    val amlPath = System.getProperty("user.dir") + "/src/test/aml/MainActivity.aml"
+    val amlPath = System.getProperty("user.dir") + File.separator + "src" + File.separator +
+            "test" + File.separator + "aml" + File.separator + "MainActivity.aml"
     val obj = AmlObject(amlPath)
 }
