847453206f8aaaa777474eda76adb93721407ad5
Vladimir Zarypov <krre31@gmail.com>
2015-03-22 17:05:21 +0200

    removed compiler warning

--- src/test/kotlin/compiler.kt
+++ src/test/kotlin/compiler.kt
@@ -1,8 +1,8 @@
 package kotloid.test.compiler
 
 import java.io.File
-import kotloid.compiler.Lexer
-import kotloid.compiler.Parser
+import org.kotloid.compiler.Lexer
+import org.kotloid.compiler.Parser
 
 fun main(args: Array<String>) {
     val amlPath = System.getProperty("user.dir") + File.separator + "src" + File.separator +
@@ -12,5 +12,6 @@ fun main(args: Array<String>) {
     val lexer = Lexer(amlPath)
     val parser = Parser(lexer)
     val ast = parser.parse()
+    println(ast)
 
 }
