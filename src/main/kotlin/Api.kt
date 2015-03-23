0dbc2e8e0f50e27ad25a9313bf38d5adad5f7183
Vladimir Zarypov <krre31@gmail.com>
2015-03-23 19:26:28 +0200

    removed command line support

---
+++ src/main/kotlin/Api.kt
@@ -0,0 +1,32 @@
+package org.kotloid
+
+import java.io.File
+
+fun compileFile(inputPath: String, outputPath: String) {
+    val ktFile = File(outputPath)
+    ktFile.getParentFile().mkdirs()
+    ktFile.createNewFile()
+
+    val kdmlText = File(inputPath).readText()
+    ktFile.writeText(kdmlText)
+}
+
+fun compileDirectory(inputPath: String, outputPath: String) {
+    deleteDirectory(File(outputPath))
+    for (file in File(inputPath).listFiles()) {
+        val ktFilePath = outputPath + File.separator + file.name.replace("kdml", "kt")
+        compileFile(file.path, ktFilePath)
+    }
+}
+
+fun deleteDirectory(path: File) {
+    if (path.exists()) {
+        for (file in path.listFiles()) {
+            if (file.isDirectory()) {
+                deleteDirectory(file)
+            } else {
+                file.delete()
+            }
+        }
+    }
+}
